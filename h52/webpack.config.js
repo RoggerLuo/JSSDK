63d50999ed2aec64e5ec5cc4093fce1ac4d7b471
Roger <luojie.5408@163.com>
2019-09-26 13:37:43 +0800

    update from air

--- h52/webpack.config.js
+++
@@ -1,2 +0,0 @@
-const dev = require('./webpack/dev.js')
-module.exports = dev
--- h52/webpack/analyze.js
+++
@@ -1,6 +0,0 @@
-const webpack = require('webpack')
-const configuration = require('./build-config.js')
-
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-configuration.plugins.push(new BundleAnalyzerPlugin())
-module.exports = configuration
